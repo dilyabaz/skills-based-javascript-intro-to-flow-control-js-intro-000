ddda7a28bf9bb1a663b43242cbbcb02762dc9e03
Dilya Joseph <dilyajoseph@gmail.com>
2020-04-26 01:07:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
-function basicTeenager = (13, 14, 15, 16, 17, 18, 19){
+if function basicTeenager = (13, 14, 15, 16, 17, 18, 19){
   ('You are a teenager!')
-} else {'undefined'}
+} else ('undefined')
 
 function teenager(age) {
 
